266e9c1d71a0838585174a3f80ecaa473ce79d61
HiroyaOoba 1-10 MacBookPro15 <ooba@1-10.coom>
2019-01-24 16:41:17 +0900

    first commit

---
+++ Assets/VFX-Graph_Test/Scripts/WebCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+
+public class WebCamera : MonoBehaviour
+{
+    int width = 640;
+    int height = 480;
+    int fps = 30;
+    WebCamTexture webcamTexture;
+    public VisualEffect vfx;
+
+
+    void Start()
+    {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        webcamTexture = new WebCamTexture(devices[0].name, this.width, this.height, this.fps);
+        webcamTexture.Play();
+        vfx.SetTexture("WebCamera", webcamTexture);
+    }
+
+    void Update()
+    {
+        // Debug.Log(webcamTexture.width + "," + webcamTexture.height);
+    }
+}
